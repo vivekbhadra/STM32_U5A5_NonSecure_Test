40a837a7f8325c06470b199daf4bef7b1937e768
Vivek Bhadra <vivek.bhadra@gmail.com>
2026-06-23 11:56:04 +0100

    Adding experimental changes.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2026 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2026 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -31,19 +31,25 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define APP_ADDRESS  0x08040000UL   /* Rust app base = FLASH_ACTIVE */
-#define ADDR_BOOT_STATE   0x08020000U
+#define APP_ADDRESS 0x08040000UL /* Rust app base = FLASH_ACTIVE */
+#define ADDR_BOOT_STATE 0x08020000U
 
 /* embassy-boot state magic constants (16-byte patterns) */
-static const uint8_t MAGIC_BOOT[16]   = {0xD0,0xD0,0xD0,0xD0,0xD0,0xD0,0xD0,0xD0,0xD0,0xD0,0xD0,0xD0,0xD0,0xD0,0xD0,0xD0};
-static const uint8_t MAGIC_SWAP[16]   = {0xD1,0xD1,0xD1,0xD1,0xD1,0xD1,0xD1,0xD1,0xD1,0xD1,0xD1,0xD1,0xD1,0xD1,0xD1,0xD1};
-static const uint8_t MAGIC_REVERT[16] = {0xD2,0xD2,0xD2,0xD2,0xD2,0xD2,0xD2,0xD2,0xD2,0xD2,0xD2,0xD2,0xD2,0xD2,0xD2,0xD2};
+static const uint8_t MAGIC_BOOT[16] = {0xD0, 0xD0, 0xD0, 0xD0, 0xD0, 0xD0,
+                                       0xD0, 0xD0, 0xD0, 0xD0, 0xD0, 0xD0,
+                                       0xD0, 0xD0, 0xD0, 0xD0};
+static const uint8_t MAGIC_SWAP[16] = {0xD1, 0xD1, 0xD1, 0xD1, 0xD1, 0xD1,
+                                       0xD1, 0xD1, 0xD1, 0xD1, 0xD1, 0xD1,
+                                       0xD1, 0xD1, 0xD1, 0xD1};
+static const uint8_t MAGIC_REVERT[16] = {0xD2, 0xD2, 0xD2, 0xD2, 0xD2, 0xD2,
+                                         0xD2, 0xD2, 0xD2, 0xD2, 0xD2, 0xD2,
+                                         0xD2, 0xD2, 0xD2, 0xD2};
 
 typedef enum {
-    BOOT_STATE_BOOT,
-    BOOT_STATE_SWAP,
-    BOOT_STATE_REVERT,
-    BOOT_STATE_UNKNOWN
+  BOOT_STATE_BOOT,
+  BOOT_STATE_SWAP,
+  BOOT_STATE_REVERT,
+  BOOT_STATE_UNKNOWN
 } BootState_t;
 /* USER CODE END PD */
 
@@ -70,11 +76,10 @@ static void JumpToApplication(uint32_t app_address);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
 
   /* USER CODE BEGIN 1 */
 
@@ -82,7 +87,8 @@ int main(void)
 
   /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick.
+   */
   HAL_Init();
 
   /* USER CODE BEGIN Init */
@@ -103,8 +109,7 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+  while (1) {
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -113,57 +118,53 @@ int main(void)
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Configure the main internal regulator output voltage
-  */
-  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE4) != HAL_OK)
-  {
+   */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE4) != HAL_OK) {
     Error_Handler();
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
   RCC_OscInitStruct.MSIState = RCC_MSI_ON;
   RCC_OscInitStruct.MSICalibrationValue = RCC_MSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_4;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
     Error_Handler();
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
-                              |RCC_CLOCKTYPE_PCLK3;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
+                                RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2 |
+                                RCC_CLOCKTYPE_PCLK3;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
   RCC_ClkInitStruct.APB3CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-  {
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
     Error_Handler();
   }
 }
 
 /* USER CODE BEGIN 4 */
-static void JumpToApplication(uint32_t app_address)
-{
+static void JumpToApplication(uint32_t app_address) {
   typedef void (*pFunction)(void);
 
-  uint32_t app_sp = *((volatile uint32_t *)(app_address));      /* initial MSP */
-  uint32_t app_pc = *((volatile uint32_t *)(app_address + 4U)); /* reset vector */
+  uint32_t app_sp = *((volatile uint32_t *)(app_address)); /* initial MSP */
+  uint32_t app_pc =
+      *((volatile uint32_t *)(app_address + 4U)); /* reset vector */
   pFunction app_reset_handler = (pFunction)app_pc;
 
   /* 1. Disable all interrupts */
@@ -172,7 +173,7 @@ static void JumpToApplication(uint32_t app_address)
   /* 2. Stop SysTick */
   SysTick->CTRL = 0;
   SysTick->LOAD = 0;
-  SysTick->VAL  = 0;
+  SysTick->VAL = 0;
 
   /* 3. Disable & clear all NVIC interrupts */
   for (uint32_t i = 0; i < 8U; i++) {
@@ -180,9 +181,16 @@ static void JumpToApplication(uint32_t app_address)
     NVIC->ICPR[i] = 0xFFFFFFFFUL;
   }
 
-  /* 4. De-init HAL/clocks -> back to reset (MSI) so Embassy can reconfigure RCC */
+  /* 4. De-init HAL/clocks -> back to reset (MSI) so Embassy can reconfigure RCC
+   */
   HAL_RCC_DeInit();
   HAL_DeInit();
+
+  /* Before the barriers/jump, after HAL_DeInit(): */
+  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /* wait for VOSRDY */
+  while (!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {
+  }
 
   /* 5. Relocate vector table to the application */
   SCB->VTOR = app_address;
@@ -197,37 +205,36 @@ static void JumpToApplication(uint32_t app_address)
 
   app_reset_handler();
 
-  while (1) { }  /* never returns */
+  while (1) {
+  } /* never returns */
 }
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
-  while (1)
-  {
+  while (1) {
   }
   /* USER CODE END Error_Handler_Debug */
 }
 #ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t *file, uint32_t line) {
   /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* User can add his own implementation to report the file name and line
+     number, ex: printf("Wrong parameters value: file %s on line %d\r\n", file,
+     line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
